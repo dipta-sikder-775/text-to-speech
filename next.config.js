1a334a39aad8eb58f3910976a0a3e6c44e8d3c40
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-07 11:07:36 +0600

    - build error solved

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   env: {
     BASE_API_URL: "https://text-to-speech-api3.p.rapidapi.com",
     "X-RapidAPI-Key": "aa99057212msh0319760089ff124p183a2fjsnce313ea5e2f8",
